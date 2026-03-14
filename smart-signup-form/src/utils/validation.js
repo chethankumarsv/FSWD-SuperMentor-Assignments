78fec6a053d9fe94217715f9feaa52bbada06780
Chethan Kumar SV <chethansv2345@gmail.com>
2026-03-14 19:20:57 +0530

    Add files via upload

---
+++ smart-signup-form/src/utils/validation.js
@@ -0,0 +1,10 @@
+export const validateEmail = (email) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+};
+
+export const validatePassword = (password) => {
+  const regex =
+    /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
+  return regex.test(password);
+};
